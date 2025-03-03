1c8cd6b4cb613fb6febdad65c536b9101bbd8718
vuartex <165473461+vuartex@users.noreply.github.com>
2025-03-03 16:37:28 +0300

    Create background.js

---
+++ background.js
@@ -0,0 +1,22 @@
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.sync.set({ autoplay: true, pauseOnTabChange: false, disableAll: false });
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === "getSettings") {
+        chrome.storage.sync.get(["autoplay", "pauseOnTabChange", "disableAll"], (data) => {
+            sendResponse(data);
+        });
+        return true;
+    } else if (request.action === "updateSettings") {
+        chrome.storage.sync.set(request.settings);
+    }
+});
+
+chrome.tabs.onActivated.addListener((activeInfo) => {
+    chrome.storage.sync.get("pauseOnTabChange", (data) => {
+        if (data.pauseOnTabChange) {
+            chrome.tabs.sendMessage(activeInfo.tabId, { action: "pauseVideo" });
+        }
+    });
+});
