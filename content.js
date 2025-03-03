5bcc24f1e8b34c3360d691a5d9fa140ce4bbe5ed
vuartex <165473461+vuartex@users.noreply.github.com>
2025-03-03 16:37:02 +0300

    Update content.js

--- content.js
+++ content.js
@@ -1,6 +1,21 @@
 setInterval(() => {
-    let video = document.querySelector("video");
-    if (video && video.paused) {
-        video.play();
-    }
+    chrome.runtime.sendMessage({ action: "getSettings" }, (data) => {
+        if (data.disableAll) return; // Eğer uzantı kapatılmışsa hiçbir işlem yapma
+        
+        let video = document.querySelector("video");
+        if (video) {
+            if (data.autoplay && video.paused) {
+                video.play();
+            }
+        }
+    });
 }, 1000);
+
+chrome.runtime.onMessage.addListener((request) => {
+    if (request.action === "pauseVideo") {
+        let video = document.querySelector("video");
+        if (video) {
+            video.pause();
+        }
+    }
+});
